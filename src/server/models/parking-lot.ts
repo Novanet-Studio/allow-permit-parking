d0d15128824338ef00989fe489fe3189b58de1fc
David Arenas <davejs136@gmail.com>
2021-05-17 11:38:02 -0400

    add: parking lot model

---
+++ src/server/models/parking-lot.ts
@@ -0,0 +1,30 @@
+import { Document, Schema, model, Model } from 'mongoose';
+
+export interface ParkingLot extends Document {
+  name: string;
+}
+
+export const ParkingLotSchema = new Schema<
+  ParkingLot,
+  Model<ParkingLot, ParkingLot>
+>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+ParkingLotSchema.methods.toJSON = function (): Thruway.ParkingLot {
+  return {
+    name: this.name,
+  };
+};
+
+const ParkingLotModel = model<ParkingLot>('User', ParkingLotSchema);
+
+export default ParkingLotModel;
