3e936b67dd249f075a32fd994791c760ebb20d26
David Arenas <davejs136@gmail.com>
2021-07-06 15:24:48 -0400

    fix: back breadcrumb functionality

--- src/client/components/Breadcrumb/index.tsx
+++ src/client/components/Breadcrumb/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 import backIcon from '../../assets/images/app_icon_back.svg';
 
@@ -7,10 +8,14 @@ type Props = {
 };
 
 export default function Breadcrumb({ text = 'Back' }: Props): JSX.Element {
+  const router = useRouter();
+
+  const handleBack = () => router.replace('/');
+
   return (
-    <a href="/" className="breadcrumb">
+    <div className="breadcrumb" onClick={handleBack}>
       <img className="breadcrumb__icon" src={backIcon} alt="back icon" />
       <p className="breadcrumb__text">{text}</p>
-    </a>
+    </div>
   );
 }
